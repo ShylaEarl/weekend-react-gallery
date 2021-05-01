e97106f3c132fb88d3f4acdfcd7c40ed6f4628ae
Shyla Earl <shyla.earl@gmail.com>
2021-05-01 15:59:10 -0500

    code cleaned up, auto formatted

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,17 +5,17 @@ import GalleryList from '../GalleryList/GalleryList.jsx';
 
 function App() {
 
-    let [galleryList, setGalleryList] = useState([]);
+  let [galleryList, setGalleryList] = useState([]);
 
-    //on page load, render data array to DOM
-    useEffect(() => {
-      //console.log('in useEffect');
-      getImage();
-    }, []);
+  //on page load, render data array to DOM
+  useEffect(() => {
+    //console.log('in useEffect');
+    getImage();
+  }, []);
 
-    //GET request getting data from server
-    const getImage = () => {
-      axios.get('/gallery')
+  //GET request getting data from server
+  const getImage = () => {
+    axios.get('/gallery')
       .then((response) => {
         //console.log('response from getImage', response.data);
         //assigns response data to an array
@@ -24,33 +24,17 @@ function App() {
       .catch(error => {
         console.log(error);
       });
-    }
+  }
 
-    // //PUT request to update like counter (logic in router)
-    // const updateLikes = (image) => {
-    //   // let id = event.currentTarget.dataset.id;
-    //   console.log('like button clicked for image:', image);
-
-    //   axios.put(`/gallery/like/${image.id}`, image) 
-    //   .then((response) => {
-    //     console.log('in PUT', response);
-    //     getImage(); 
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   })
-    // }
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <GalleryList galleryList={galleryList} getImage={getImage}/>
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <GalleryList galleryList={galleryList} getImage={getImage} />
+    </div>
+  );
 }
 
 export default App;
 
-//updateLikes={updateLikes}
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,60 +1,44 @@
 import './GalleryItem.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
-function GalleryItem(props){
-    console.log('gallery item props are:', props);
-    
-    //const image = props.image;
+function GalleryItem(props) {
+    //console.log('gallery item props are:', props);
 
     const [swap, setSwap] = useState(true);
 
     const imageSwap = () => {
         setSwap(!swap);
-        console.log('image clicked');
+        //console.log('image clicked');
     }
 
     //PUT request to update like counter (logic in router)
     const updateLikes = (event) => {
         let id = event.currentTarget.dataset.id;
         console.log('like button clicked for image:', id);
-  
-        axios.put(`/gallery/like/${id}`) 
-        .then((response) => {
-          console.log('in PUT', response);
-          props.getImage(); 
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-      }
 
-    // const likeMessage = () => {
-    //     updateLikes(image.id);
-    //     if(image.likes === 1){
-    //         return (<p>{image.likes} person loves this!</p>)
-    //     } else {
-    //         return (<p>{image.likes} people love this!</p>)
-    //     }
-    // }
+        axios.put(`/gallery/like/${id}`)
+            .then((response) => {
+                //console.log('in PUT', response);
+                props.getImage();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-    return(
+    return (
         <div className="image" >
             {swap && <img onClick={imageSwap} src={props.path} alt={props.description} width='150' />}
             {!swap && <div onClick={imageSwap} className="description">{props.description}</div>}
             <br></br>
-            <button onClick={updateLikes} data-id={props.id}>Love it!</button>
+            <button onClick={updateLikes} data-id={props.id}>I Like It!</button>
             <br></br>
-            <p>{props.likes}</p>
-            
+            <p>Likes: {props.likes}</p>
         </div>
     )
 }
 
 export default GalleryItem;
 
-//() => props.updateLikes(image)
-//likeMessage
-//data-id={image.id}
-//() => props.updateLikes(image.id)
 
